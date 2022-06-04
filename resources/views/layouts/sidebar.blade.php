36128d8870d3faae91416e8ef4bf1fe2dcded5d7
tiosetiawan <tiosetiawan11@gmail.com>
2022-06-04 14:31:13 +0800

    update nav active

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,15 +3,15 @@
 		<a class="toggle back" id="sidenav-toggle"><i class="fa fa-angle-left"></i></a>
 		<a class="avatar-username" href="/"> <img class="img-responsive logo-sidebar center-block" width="80" height="30" src="/img/logo-imip.png"></a>
 	</div>
-	<ul class="sidenav-menu">
+	<ul class="navbar-nav sidenav-menu">
         @php
          $menus = menus()
         @endphp
         @foreach ($menus as $menu)
-        <li>
+        <li class="nav-item {{ Request::is(strtolower($menu['menu'])) ? 'active' : '' }}">
             <a href="{{ $menu['route_name'] }}">
-                <span class="sidenav-link-icon"> <i class="{{ $menu['icon'] }}"></i></span>
-                </span><span class="sidenav-link-title">{{ $menu['menu'] }}</span>
+                <span class="sidenav-link-icon"><i class="{{ $menu['icon'] }}"></i></span>
+                <span class="sidenav-link-title">{{ $menu['menu'] }}</span>
             </a>
         </li>
         @endforeach  
