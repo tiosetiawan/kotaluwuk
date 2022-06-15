c4ad26815e0f5f436153a793304232dd4d5a0436
tiosetiawan <tiosetiawan11@gmail.com>
2022-06-15 13:31:13 +0800

    perbaikan loading process

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,13 +2,13 @@
 <div class="container-fluid">
 	<br><hr class="footer">
 	<p align="center">{{ ENV('APP_FOOTER') }}</p>
-	{{-- <p align="center">Version  - Page rendered in <b>{{ microtime(true) }}</b> second </p> --}}
 </div>
 
 <div id="loading"> 
 	<div class="loader">
-        {{-- <img src="<?php echo base_url('img/loading.gif'); ?>"> --}}
-        <label>Data sedang di proses<label>
+        <img src="{{ asset('/img/loading-new.gif') }}" width="100px">
+        <label>Loading<label>
     </div> 
+
 	<div class="overlay"></div>
 </div>
